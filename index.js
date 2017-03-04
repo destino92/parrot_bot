2a052a43de23318e7960fe9fb0a89d71c42f323a
destino92 <destinodello@gmail.com>
2017-03-04 23:50:42 +0300

    fix: update function to echo back message url

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ function sendTextMessage(sender, text) {
         text:text
     }
     request({
-        url: 'https://graph.facebook.com/v2.8/me/...',
+        url: 'https://graph.facebook.com/v2.8/1761827604131497/...',
         qs: {access_token:token},
         method: 'POST',
         json: {
@@ -144,7 +144,7 @@ function sendGenericMessage(sender) {
         }
     }
     request({
-        url: 'https://graph.facebook.com/v2.8/me/...',
+        url: 'https://graph.facebook.com/v2.8/1761827604131497/...',
         qs: {access_token:token},
         method: 'POST',
         json: {
